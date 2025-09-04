63a28552ceb8b7c36a3b52caa44e1a9bd0490284
Chidumam Amadi-Obi <amadiobichidumam@gmail.com>
2025-09-04 19:36:06 +0100

    Update Calibration.h

--- Tests/TestLDR/Calibration.h
+++ Tests/TestLDR/Calibration.h
@@ -1,17 +1,17 @@
 // https://www.allaboutcircuits.com/projects/design-a-luxmeter-using-a-light-dependent-resistor/ 
 
-#define LDR_PIN                   26   // LDR connected to analoug input pin
-#define MAX_ADC_READING           4095 // already set - leave this alone.
+#define LDR_PIN                   4   // LDR connected to analoug input pin
+#define MAX_ADC_READING           4095 
 #define ADC_REF_VOLTAGE           5.05 // Voltage supply to the voltage divider.. (Measure it)
-#define REF_RESISTANCE            10000  // measure this for best results, Fergus is using a 10K resistor so set this to 10K
-#define LUX_CALC_SCALAR           2355175 //from Excell sheet - scalar (10^(y-intercept))  - This Y intecept is found by experimentation using the light box and a lux meter. I need to develope a lab sheet for this.
-#define LUX_CALC_EXPONENT         -1.2109   // From Excell sheet - Slope of plot - from Excel
+#define REF_RESISTANCE            10000 
+#define LUX_CALC_SCALAR           2355175
+#define LUX_CALC_EXPONENT         -1.2109   
 
-float Lux_Value() {
-  int   ldrRawData;
+int Lux_Value() {
+  uint16_t   ldrRawData;
   float resistorVoltage, ldrVoltage;
   float ldrResistance;
-  float ldrLux;
+  int ldrLux;
  
   ldrRawData = analogRead(LDR_PIN);
   resistorVoltage = (float)ldrRawData / MAX_ADC_READING * ADC_REF_VOLTAGE;
@@ -26,5 +26,6 @@ float Lux_Value() {
  
   return(ldrLux);
 }
+
 
 
