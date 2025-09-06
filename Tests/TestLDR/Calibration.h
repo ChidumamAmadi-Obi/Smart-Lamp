f4acea216d647b63e518d2efa8b9057e0972b237
Chidumam Amadi-Obi <amadiobichidumam@gmail.com>
2025-09-06 01:36:05 +0100

    Filtered out ADC noise

--- Tests/TestLDR/Calibration.h
+++ Tests/TestLDR/Calibration.h
@@ -6,26 +6,47 @@
 #define REF_RESISTANCE            10000 
 #define LUX_CALC_SCALAR           2355175
 #define LUX_CALC_EXPONENT         -1.2109   
+#define MAX_LDR_READING           70000
+#define LUX_EMA_ALPHA 0.3
 
-int Lux_Value() {
+static float emaLux = 0;
+static bool emaInitialized = false;
+
+float exponentialMovingAverage(float newVal) {
+  if (!emaInitialized) {
+    emaLux = newVal;
+    emaInitialized = true;
+    return newVal;
+  }
+  emaLux = (LUX_EMA_ALPHA * newVal) + ((1 - LUX_EMA_ALPHA) * emaLux);
+  return emaLux;
+}
+float Lux_Value() {
   uint16_t   ldrRawData;
   float resistorVoltage, ldrVoltage;
   float ldrResistance;
-  int ldrLux;
+  float rawLdrLux;
+  float filteredLdrLux;
  
   ldrRawData = analogRead(LDR_PIN);
   resistorVoltage = (float)ldrRawData / MAX_ADC_READING * ADC_REF_VOLTAGE;
   ldrVoltage = ADC_REF_VOLTAGE - resistorVoltage;
   ldrResistance = ldrVoltage/resistorVoltage * REF_RESISTANCE;
-  ldrLux = LUX_CALC_SCALAR * pow(ldrResistance, LUX_CALC_EXPONENT);   // This is the formula that calculates Lux... See lab notes that I havent written yet(20/2/2020)!!!!!
+  rawLdrLux = LUX_CALC_SCALAR * pow(ldrResistance, LUX_CALC_EXPONENT);   // This is the formula that calculates Lux... See lab notes that I havent written yet(20/2/2020)!!!!!
 
+  if (rawLdrLux > MAX_LDR_READING) rawLdrLux = MAX_LDR_READING;
+
+  filteredLdrLux = exponentialMovingAverage(rawLdrLux);
+  
   //Serial.print("LDR Raw Data   : "); Serial.println(ldrRawData);
   //Serial.print("LDR Voltage    : "); Serial.print(ldrVoltage); Serial.println(" volts");
   //Serial.print("LDR Resistance : "); Serial.print(ldrResistance); Serial.println(" Ohms");
-  Serial.print("LDR Illuminance: "); Serial.print(ldrLux); Serial.println(" lux");
- 
-  return(ldrLux);
+  Serial.print("LDR unfiltered :"); Serial.print(rawLdrLux);
+  Serial.print(" / LDR filtered   :"); Serial.println(filteredLdrLux);
+  
+  return(filteredLdrLux);
 }
+
 
 
 
